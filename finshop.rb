0306341bb4b7fdedbeaef9cc7b5a0b70a2071ad6
Simon Schoeters <hamfilter@gmail.com>
2017-09-29 20:58:02 +0200

    Notify via Prowl or stdout

--- finshop.rb
+++ finshop.rb
@@ -13,6 +13,5 @@ store    = Store.new
 store.upsert(products)
 
 ten_minutes_ago = Time.now - 600
-if store.since(ten_minutes_ago).any?
-  Notifier.notify("New products available!")
-end
+recent_products = store.since(ten_minutes_ago)
+Notifier.new(recent_products).notify(:prowl)
--- notifier.rb
+++ notifier.rb
@@ -1,14 +1,41 @@
 require "net/https"
 
+
 class Notifier
   PROWL_APPLICATION = "FinShop"
   PROWL_URL         = URI("https://api.prowlapp.com/publicapi/add")
-  PROWL_KEY         = ENV.fetch("PROWL_KEY")
+  PROWL_KEY         = ENV["PROWL_KEY"]
 
-  def self.notify(message)
+  def initialize(records)
+    @records = records
+  end
+
+  def notify(channel = :stdout)
+    return if @records.empty?
+
+    message = "New products found:\n"
+    message += @records.map(&:title).join("\n")
+
+    case channel
+      when :prowl then notify_prowl(message)
+      else notify_stdout(message)
+    end
+  end
+
+  def notify_prowl(message)
+    raise MissingKeyError unless PROWL_KEY
     Net::HTTP.post_form(PROWL_URL,
                         apikey: PROWL_KEY,
                         application: PROWL_APPLICATION,
                         description: message)
+  rescue MissingKeyError
+    puts "Missing PROWL_KEY, falling back to stdout."
+    notify_stdout(message)
+  end
+
+  def notify_stdout(message)
+    puts message
   end
 end
+
+class MissingKeyError < StandardError; end
